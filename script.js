f8bdcd3bc60bb0a2b3407c3e68185e2aa67f7bac
mf.humam <150023514+fawwazhumam@users.noreply.github.com>
2025-05-03 06:21:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+// *Method DOM
+
+// *selection -> getElementById()
+
+
+// *selection -> getElementByTagName()
+
+
+// *Selection -> getElementByClassName()*
+
+
+// *Selection -> querySelector()
+
+
+// *Selection -> querySelectorAll()
+
+
+// *# Mengubah Node Root
+
+// *
+
+
+// *Manipulation Element:
+
+
+// *Manipulation -> innerHTML
+
+
+// *Manipulation -> backgroundColor
+
+
+// *Manipulation -> setAttribute
+
+
+// *Menggunakan -> setAttribute
+
+
+// *Menggunakan -> getAttribute
+
+
+// *Menggunakan -> removeAttribute
+
+
+// *Manipulation -> classList
+
+
+// *Menggunakan -> toggle
+
+
+// *Manipulation Node
+
+
+// *Manipulation Node: -> createElement -> createTextNode -> appendChild
+
+
+// *Manipulation -> Membuat Node Baru / Team Baru(studicase)
+
+
+// *Manipulation -> Menyeleksi li didalam ul -> Menambahkan listNew sebelum -> list2
+
+
+// *Manipulation -> replace and remove
+
+
+// *Manipulation -> remove -> remove tag "<a></a>""
+
+
+// *Manipulation -> replace -> class p1(humam) -> class p1(User Baru)
+
+
+// *Event Handler & Event Listener (DOM Event)
+
+
+// *Event Handler -> inlineHTML -> onClick="your_function"
+
+
+// *Create Event Handler for -> btn_1
+
+
+// *Event Handler -> event dalam script
+
+
+// *Create Event Handler untuk -> btn_2
+
+
+// *Membuat function clearAll() {
+
+
+// *Method addEventListener
+
+
+// *addEventListener -> h1 -> create alert
+
+
+// *addEventListener -> h1 -> create button -> New User
+
+
+// *Event Handler VS Event Listener
+
+
+// *eventHandler
+
+
+// *eventListener
